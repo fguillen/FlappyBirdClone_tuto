06f9f615c07d4a8c76e0f5fa6cd52b903fb9d214
Fernando Guillen <fguillen.mail@gmail.com>
2014-06-07 17:01:06 +0200

    It works :)

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,25 +7,38 @@ var game = new Phaser.Game(400, 490, Phaser.AUTO, 'game_div');
 var main_state = {
 
   preload: function() {
-    // Change the background color of the game
     this.game.stage.backgroundColor = '#71c5cf';
 
-    // Load the bird sprite
     this.game.load.image('bird', '/images/bird.png');
+    this.game.load.image('pipe', '/images/pipe.png');
   },
 
   create: function() {
-    // Display the bird on the screen
+    // Looks like I don't need this bellow line
+    // this.game.physics.startSystem(Phaser.Physics.ARCADE);
+
     this.bird = this.game.add.sprite(100, 245, 'bird');
-
-    console.log("bird", this.bird);
-
-    // Add gravity to the bird to make it fall
+    this.game.physics.enable( [ this.bird ], Phaser.Physics.ARCADE);
     this.bird.body.gravity.y = 1000;
+
+    this.pipes = game.add.group();
+    this.pipes.createMultiple(20, 'pipe');
+
+    this.pipes.forEach( function(pipe){
+      this.game.physics.enable( [ pipe ], Phaser.Physics.ARCADE);
+    });
 
     // Call the 'jump' function when the spacekey is hit
     var space_key = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
     space_key.onDown.add(this.jump, this);
+
+    // Add pipes after 1500ms
+    this.timer = this.game.time.events.loop(1500, this.add_row_of_pipes, this);
+
+    // Score
+    this.score = 0;
+    var style = { font: "30px Arial", fill: "#ffffff" };
+    this.label_score = this.game.add.text(20, 20, "0", style);
   },
 
   update: function() {
@@ -33,6 +46,25 @@ var main_state = {
     if (this.bird.inWorld == false) {
       this.restart_game();
     }
+
+    // Looks like I have to kill the pipe on my own when it is out of the canvas
+    this.pipes.forEach( function(pipe){
+      if (pipe.alive && !pipe.inWorld) {
+        pipe.kill();
+      }
+    });
+
+    this.game.physics.arcade.overlap(
+      this.bird,
+      this.pipes,
+      this.restart_game,
+      null,
+      this
+    );
+  },
+
+  render: function(){
+
   },
 
   // Make the bird jump
@@ -43,8 +75,38 @@ var main_state = {
 
   // Restart the game
   restart_game: function() {
+    this.game.time.events.remove(this.timer);
+
     // Start the 'main' state, which restarts the game
     this.game.state.start('main');
+  },
+
+  add_one_pipe: function(x, y) {
+    console.log(this.pipes.countDead());
+    // Get the first dead pipe of our group
+    var pipe = this.pipes.getFirstDead();
+
+    // Set the new position of the pipe
+    pipe.reset(x, y);
+
+    // Add velocity to the pipe to make it move left
+    pipe.body.velocity.x = -200;
+
+    // Kill the pipe when it's no longer visible
+    pipe.outOfBoundsKill = true;
+  },
+
+  add_row_of_pipes: function() {
+    var hole = Math.floor(Math.random()*4)+1;
+
+    for (var i = 0; i < 8; i++) {
+      if (i != hole && i != hole +1 && i != hole +2) {
+        this.add_one_pipe(400, i*60+10);
+      }
+    }
+
+    this.score += 1;
+    this.label_score.setText(this.score);
   },
 };
 
